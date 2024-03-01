5bd7778c13444113ea9ea0905aab92351b6cc94e
vrk0ranit <ranitbag49@gmail.com>
2024-03-01 11:16:05 +0530

    jwt token

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,46 +1,67 @@
-const express = require("express")
-const path = require("path")
-const User = require("../model/user")
+const express = require("express");
+const path = require("path");
+const User = require("../model/user");
 const router = express.Router();
-const {upload} = require("../multer");
+const { upload } = require("../multer");
 const ErrorHandler = require("../utils/ErrorHandler");
-const fs =require("fs")
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
 
-router.post("/create-user", upload.single("file"), async(req,res, next) => {
-    const {name,email,password} =req.body;
-        const userEmail = await User.findOne({email});
-    if(userEmail){
-          const filename = req.file.filename;
-          const filePath = `uploads/${filename}`
-          fs.inlink(filePath, (err) => {
-            if(err){
-                console.log(err);
-                res.status(500).json({message: "Error deleting file"})
-            } else{
-                res.json({message: "File deleted successfully"})
-            }
-          })
-          return next(new ErrorHandler("User already exist", 400))
-    }
-     
-    const filename = req.file.filename;
-    const fileUrl = path.join(filename)
-    
+router.post("/create-user", upload.single("file"), async (req, res, next) => {
+    try {
+        const { name, email, password } = req.body;
+        const userEmail = await User.findOne({ email });
 
-    const user = {
-        name: name,
-        email: email,
-        password: password,
-        avatar: {
-            url: fileUrl,
-            public_id:"some",
+        if (userEmail) {
+            const filename = req.file.filename;
+            const filePath = `uploads/${filename}`
+            fs.unlink(filePath, (err) => {
+                if(err){
+                    console.log(err);
+                    res.status(500).json({message: "Error deleting files"})
+                }else{
+                    res.json({message: "File deleted successfully"})
+                }
+            })
+           return next(new ErrorHandler("User already exist", 400))
         }
-    };
-    const newUser = await User.create(user);
-    res.status(201).json({
-        success: true,
-        newUser,
-    })
-})
 
-module.exports = router
+        const filename = req.file.filename;
+        const fileUrl = path.join(filename);
+
+        const user = {
+            name: name,
+            email: email,
+            password: password,
+            avatar: {
+                url: fileUrl,
+                public_id: "some",
+            },
+        };
+
+       const activationToken = createActivationToken(user);
+       const activationUrl = `http://localhost:3000/activation/${activationToken}`
+
+       try {
+        
+       } catch (error) {
+        return next(new ErrorHandler(error.message, 500))
+       }
+
+    } catch (error) {
+        return next(new ErrorHandler(error.message, 400));
+    }
+});
+
+//create activation token
+
+const createActivationToken = (user) => {
+    return jwt.sign(user, process.env.ACTIVATION_SECRET,{
+        expiresIn: "5m"
+    })
+} 
+
+//activate user
+
+
+module.exports = router;
--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import {AiOutlineEye, AiOutlineEyeInvisible} from "react-icons/ai"
 import styles from "../../styles/styles.js"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import {RxAvatar} from "react-icons/rx"
 import axios from "axios"
 import {server} from "../../server.js"
@@ -12,6 +12,7 @@ const Signup = () => {
   const [visible, setVisible] = useState(false)
   const [name, setName] = useState("")
   const [avatar, setAvatar] = useState(null)
+  const navigate = useNavigate();
   
   
 
@@ -31,7 +32,9 @@ const Signup = () => {
     newForm.append("password", password)
   
       axios.post(`${server}/user/create-user`, newForm, config).then((res) => {
-        console.log(res);
+        if(res.data.success === true){
+          navigate("/");
+        }
       }).catch((err) => {
         console.log(err);
       });
